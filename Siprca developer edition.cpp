d86157a76b8aeea1ba73263f356f867f102d4739
DDABBCCFFGG <140588675+DDABBCCFFGG@users.noreply.github.com>
2024-02-22 15:45:15 +0800

    Siprca developer  editon V.0.1.0
    This is Siprca developer  editon V.0.1.0
    (Preview)
    If you have good suggestion,you can write in Issue.
    Welcome to use! ^_^

---
+++ Siprca developer edition.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+double n,m;
+char t;
+int main()
+{
+	cin >> n >> t >> m;
+	if(t == '+')
+	{
+		cout << n + m;
+	}
+	if(t == '-')
+	{
+		cout << n - m;
+	}
+	if(t == '*')
+	{
+		cout << n * m;
+	}
+	if(t == '/')
+	{
+		cout << n + m;
+	}
+	return 0;
+}
